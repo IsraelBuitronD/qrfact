e07c503ccd1bb9473b9486b888bf60dee449ee9b
Israel Buitron <israel.buitron@gmail.com>
2010-08-24 11:18:44 -0500

    More square matrices functions were added.

--- src/matrix.c
+++ src/matrix.c
@@ -1,21 +1,70 @@
+#include "matrix.h"
 #include <malloc.h>
 
 void applyTranspose(double** m, int n) {
-   double tmp;
+  double tmp;
 
-   for(int i=1; i<n; i++) {
-      for(int j=0; j<i; j++) {
-         tmp = m[i][j];
-         m[i][j] = m[j][i];
-         m[j][i] = tmp;
-      }
-   }
+  for(int i=1; i<n; i++) {
+    for(int j=0; j<i; j++) {
+      tmp = m[i][j];
+      m[i][j] = m[j][i];
+      m[j][i] = tmp;
+    }
+  }
 }
 
+/*
+double** getTranspose(double** m, int n) {
+  double** transpose = (double**)calloc(n,sizeof(double*));
+
+  // TODO: implement this function
+  
+  return transpose;
+}
+*/
+
 void printSqrMat(double** m, int size) {
+  printDelimSqrMat(m,'\t',size);
+}
+
+void printDelimSqrMat(double** m, char delim, int size) {
   for(int i=0; i<size; i++) {
     for(int j=0; j<size; j++)
-      printf("%lf\t",m[i][j]);
+      printf("%lf%c",m[i][j],delim);
     printf("\n");
   }
+}
+
+int areEqualsSqrMat(double** m1, double** m2, int n) {
+  for(int i=0; i<n; i++)
+    for(int j=0; j<n; j++)
+      if(m1[i][j] != m2[i][j])
+	return NOT_EQUAL_VALUES;
+
+  return EQUAL_VALUES;
+}
+
+double** getZeroSqrMat(int size) {
+  double** mat = (double**)calloc(3,sizeof(double*));
+  for(int i=0; i<3; i++)
+    mat[i] = (double*)calloc(3,sizeof(double));
+
+  return mat;
+}
+
+double** freeSqrMat(double** m, int size) {
+  for(int i=0; i<3; i++)
+    free(m[i]);
+  free(m);
+  return m;
+}
+
+double** getIdentitySqrMat(int size) {
+  double** mat = (double**)calloc(3,sizeof(double*));
+  for(int i=0; i<3; i++) {
+    mat[i] = (double*)calloc(3,sizeof(double));
+    mat[i][i] = 1;
+  }
+
+  return mat;
 }
--- src/matrix.h
+++ src/matrix.h
@@ -1,7 +1,16 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#define EQUAL_VALUES 1
+#define NOT_EQUAL_VALUES 0
+
 void applyTranspose(double**, int);
+//double** getTranspose(double**, int);
 void printSqrMat(double**, int);
+void printDelimSqrMat(double**, char, int);
+int areEqualsSqrMat(double**, double**, int);
+double** getZeroSqrMat(int);
+double** freeSqrMat(double**, int);
+double** getIdentitySqrMat(int);
 
 #endif
